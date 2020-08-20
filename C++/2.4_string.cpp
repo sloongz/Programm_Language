e6ae92461b84a4f4a5db4ec90a28b754b2a3f3c3
sloongz <sloongz@163.com>
2020-08-19 19:07:40 -0700

    add c++ code

--- C++/2.4_string.cpp
+++ C++/2.4_string.cpp
@@ -20,10 +20,62 @@ int main()
 	const char *str = s1.c_str();
 	cout<<"c str:"<<str<<endl;
 
+	//访问字符串中的字符
 	for (unsigned int i=0; i<s4.length(); i++) {
 		cout<<s4[i]<<" ";
 	}
 	cout<<endl;
+
+
+	//字符串的拼接
+	string s5 = s1 + s4;
+	string s6 = s1 + 'A';
+	cout<<"s5: "<<s5<<"\ts6: "<<s6<<endl;
+
+	//插入字符串
+	s4.insert(6, s1);
+	cout<<"insert: "<<s4<<endl;
+	s4.insert(6, "abcde ");
+	cout<<"insert: "<<s4<<endl;
+
+	//删除字符串
+	s4.erase(6, 4);
+	cout<<"erase: "<<s4<<endl;
+	s4.erase(5);
+	cout<<"erase: "<<s4<<endl;
+
+	//提取子字符串
+	s4 = "hello world";
+	cout<<"s4: "<<s4<<endl;
+	string s7 = s4.substr(6,6);
+	cout<<"pick: "<<s7<<endl;
+
+	//字符串查找
+	string s8 = "worl";
+	unsigned int index = s4.find(s8);	
+	if(index < s4.length())
+		cout<<"find at index : "<< index <<endl;
+	else
+		cout<<"Not found"<<endl;
+
+	index = s4.find(s8,5);	
+	if(index < s4.length())
+		cout<<"find at index : "<< index <<endl;
+	else
+		cout<<"Not found"<<endl;
+
+	index = s4.rfind(s8,5);	
+	if(index < s4.length())
+		cout<<"find at index : "<< index <<endl;
+	else
+		cout<<"Not found index: "<<index<<endl;
+
+	index = s4.find_first_of(s8);
+	if(index < s4.length())
+		cout<<"find at index : "<< index <<endl;
+	else
+		cout<<"Not found index: "<<index<<endl;
+
 
 	return 0;
 }
